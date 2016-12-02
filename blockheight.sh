d1f176ee7e6fe1149f2265e06b5ffacc9c4600b1
mrgrshift <gmxmxx@gmail.com>
2016-12-02 19:54:21 +0100

    v2

--- blockheight.sh
+++ blockheight.sh
@@ -1,103 +1,210 @@
 #!/bin/bash
+#The majority of the code is from MrV's repo, thank you, you can take a look here: https://github.com/mrv777/LiskScripts/blob/master/check_height_and_rebuild.sh
+
+
 source configtoolisk.sh
 
-HEIGHT=1
-CHECKSRV=1
+HEIGHT=1	#Highest height
+CHECKSRV=1	#Local height
+SRV="127.0.0.1:8000"
+cd /home/$USERTOOL/lisk-main/
 
-first_node() {
-        ## Get height of first server
-        HEIGHT=1
-        HEIGHT=`curl -s "http://127.0.0.1:8000/api/peers"| jq '.peers[0].height'`
-        if [ -z "$HEIGHT" ]
-        then
-                HEIGHT=`curl -s "http://127.0.0.1:8000/api/peers"| jq '.peers[0].height'`
-        fi
+top_height(){
+	## Get height of your 100 peers and save the highest value
+	## Thanks to wannabe_RoteBaron for this improvement
+	HEIGHT=$(curl -s http://$SRV/api/peers | jq '.peers[].height' | sort -nu | tail -n1)
+	## Make sure height is not empty, if it is empty try the call until it is not empty
+	while [ -z "$HEIGHT" ]
+	do
+    	   sleep 1
+    	   HEIGHT=$(curl -s http://$SRV/api/peers | jq '.peers[].height' | sort -nu | tail -n1)
+	done
+
+        if ! [[ "$HEIGHT" =~ ^[0-9]+$ ]];
+            then
+                echo $SRV " " "is off?"
+                HEIGHT="0"
+            fi
 }
 
-loop_nodes() {
-        ## Get height of rest of the servers and see if any are higher
-        for i in `seq 1 99`;
-    do
-                CHECKSRV=`curl -s "http://localhost:8000/api/peers"| jq ".peers[$i].height"`
-                if [[ ${CHECKSRV+1} && "$CHECKSRV" -gt "$HEIGHT" ]];
-                then
-                        HEIGHT=$CHECKSRV
-                fi
-    done
+get_local_height(){
+	CHECKSRV=`curl -s "http://$SRV/api/loader/status/sync"| jq '.height'`
+	while [ -z "$CHECKSRV" ]
+	do
+	    	sleep 1
+		CHECKSRV=`curl -s "http://$SRV/api/loader/status/sync"| jq '.height'`
+	done
+
+        if ! [[ "$CHECKSRV" =~ ^[0-9]+$ ]];
+            then
+                echo $SRV " " "is off?"
+                CHECKSRV="0"
+            fi
+}
+
+## Thanks to cc001 and hagie for improvements here
+find_newest_snap_rebuild(){
+
+	SNAPSHOTS=(
+	  https://downloads.lisk.io/lisk/main/blockchain.db.gz	## Official
+	  https://snapshot.liskwallet.net/blockchain.db.gz		## isabella
+	  https://snapshot.lisknode.io/blockchain.db.gz			## Gr33nDrag0n
+	  https://lisktools.io/backups/blockchain.db.gz			## MrV
+	  https://snapshot.punkrock.me/blockchain.db.gz			## punkrock
+	  https://snapshot.lsknode.org/blockchain.db.gz			## redsn0w
+	)
+
+	BESTSNAP=""
+	BESTTIMESTAMP=0
+	BESTSNAPLENGTH=0
+
+	for SNAP in ${SNAPSHOTS[@]}
+	do
+	  echo "$SNAP"
+	  SNAPSTATUS=$(curl -sI "$SNAP" | grep "HTTP" | cut -f2 -d" ")
+	  SNAPLENGTH=$(curl -sI "$SNAP" | grep "Length" | cut -f2 -d" ")
+	  SNAPLENGTH="${SNAPLENGTH//[$'\t\r\n ']}"
+	  if [ "$SNAPSTATUS" -eq "200" ]
+	  then
+		  TIME=$(curl -sI "$SNAP" | grep Last-Modified | cut -f2 -d:)
+		  TIMESTAMP=$(date -d "$TIME" +"%s")
+		  echo $TIMESTAMP
+		  if [ "$TIMESTAMP" -gt "$BESTTIMESTAMP" ] && [ "$SNAPLENGTH" -gt "$BESTSNAPLENGTH" ]; ## Make sure it is the newest and the largest
+		  then
+			 BESTSNAP=$SNAP
+			 BESTTIMESTAMP=$TIMESTAMP
+			 BESTSNAPLENGTH=$SNAPLENGTH
+		  fi
+	   fi
+	done
+
+    REPO=${BESTSNAP%/blockchain.db.gz}
+	echo "Newest snap: $BESTSNAP | Rebuilding from $REPO"
+
+    ## bash lisk.sh stop ## Trying to figure out why rebuilding from block 0.  Attempting to stop first to make sure the DB shuts down too
+    ## sleep 5
+    bash lisk.sh rebuild -u $REPO
 }
 
 rebuild_alert(){
                 TIME=$(date +"%H:%M") #add for your local time: -d '6 hours ago')
                 #failover script
-                curl -s -k -H "Content-Type: application/json" -X POST -d "{\"secret\":\"$SECRET\"}" $URL_LOCAL_DISABLE
                 RESPONSE=$(curl -s -k -H "Content-Type: application/json" -X POST -d "{\"secret\":\"$SECRET\"}" $URL_REMOTE)
+                curl -s -k -H "Content-Type: application/json" -X POST -d "{\"secret\":\"$SECRET\"}" $URL_LOCAL_DISABLE
                 echo $RESPONSE | grep "true" > /dev/null
                if [ $? = 0 ]; then
                    echo -e "${GREEN}Forging activated successfully${OFF}"
 
-                   MG_SUBJECT="$DELEGATE_NAME rebuild started Forging enabled successfully"
-                   MG_TEXT="$DELEGATE_NAME rebuild started $HEIGHT - Forging enabled successfully $RESPONSE"
+                   MG_SUBJECT="$DELEGATE_NAME rebuild started Forging enabled successfully in $IP_SERVER"
+                   MG_TEXT="$DELEGATE_NAME rebuild started Highest: $HEIGHT - Local: $CHECKSRV Forging enabled successfully in $IP_SERVER  $RESPONSE"
                    echo "Forging enabled > $RESPONSE" >> $BLOCKHEIGHT_LOG
                 else
                    echo -e "${RED}Forging not enabled!${OFF} - $RESPONSE"
                    echo "Forging NOT enabled! > $RESPONSE" >> $BLOCKHEIGHT_LOG
                    MG_SUBJECT="$DELEGATE_NAME Forging not enabled!"
-                   MG_TEXT="$DELEGATE_NAME rebuild started $HEIGHT - Forging not enabled! $RESPONSE"
+                   MG_TEXT="$DELEGATE_NAME rebuild started Highest: $HEIGHT - Local: $CHECKSRV Forging not enabled! $RESPONSE"
                 fi
 
                 echo "Starting rebuild at $TIME"
                 echo "Starting rebuild at $TIME" >> $BLOCKHEIGHT_LOG
-                MG_SUBJECT="$DELEGATE_NAME is in $CHECKSRV height, rebuild started"
-                MG_TEXT="$DELEGATE_NAME is in $CHECKSRV height, rebuild started"
                 curl -s --user "api:$API_KEY" $MAILGUN -F from="$MG_FROM" -F to="$MG_TO" -F subject="$MG_SUBJECT" -F text="$MG_TEXT"
 }
+
 
 post_rebuild(){
                 echo "Rebuild ended.."
                 echo "Rebuild ended.." >> $BLOCKHEIGHT_LOG
+		top_height
+		get_local_height
 
-                HEIGHT=`curl -s "http://127.0.0.1:8000/api/loader/status/sync"| jq '.height'`
-                if [ -z "$CHECKSRV" ]
-                then
-                        HEIGHT=`curl -s "http://127.0.0.1:8000/api/loader/status/sync"| jq '.height'`
-                fi
-
-
-                MG_SUBJECT="$DELEGATE_NAME synchronization ended $HEIGHT"
-                MG_TEXT="$DELEGATE_NAME synchronization ended $HEIGHT"
+                MG_SUBJECT="$DELEGATE_NAME synchronization ended Highest: $HEIGHT - Local: $CHECKSRV"
+                MG_TEXT="$DELEGATE_NAME synchronization ended Highest: $HEIGHT - rebuild: $CHECKSRV"
                 curl -s --user "api:$API_KEY" $MAILGUN -F from="$MG_FROM" -F to="$MG_TO" -F subject="$MG_SUBJECT" -F text="$MG_TEXT"
 }
 
+## Thank you corsaro for this improvement
+check_if_rebuild_finish(){
+	while true; do
+		s1=`curl -k -s "http://127.0.0.1:8000/api/loader/status/sync"| jq '.height'`
+		sleep 60
+		s2=`curl -k -s "http://127.0.0.1:8000/api/loader/status/sync"| jq '.height'`
+
+		diff=$(( $s2 - $s1 ))
+		if [ "$diff" -gt "10" ]
+		then
+		   echo "$s2 is too greater then $s1"
+		   echo "it looks like rebuild not finished yet. we have not to stop"
+		else
+		   echo "$s1 - $s2"
+		   echo "looks like rebuilding finished. we can stop this"
+		   break
+		fi
+	done
+}
+
+start_reload(){
+	TIME=$(date +"%H:%M") #add for your local time: -d '6 hours ago')
+	echo "Starting reload at $TIME"
+
+
+        bash lisk.sh reload
+        sleep 60
+        echo "Reload finished"
+}
+
+found_fork_alert(){
+	FORK=$1
+	echo "Found fork: $FORK" >> $BLOCKHEIGHT_LOG
+        TIME=$(date +"%H:%M") #add for your local time: -d '6 hours ago')
+        echo "Starting reload at $TIME"
+                #failover script
+                RESPONSE=$(curl -s -k -H "Content-Type: application/json" -X POST -d "{\"secret\":\"$SECRET\"}" $URL_REMOTE)
+                curl -s -k -H "Content-Type: application/json" -X POST -d "{\"secret\":\"$SECRET\"}" $URL_LOCAL_DISABLE
+                echo $RESPONSE | grep "true" > /dev/null
+               if [ $? = 0 ]; then
+                   echo -e "${GREEN}Forging activated successfully${OFF}"
+
+                   MG_SUBJECT="$DELEGATE_NAME reload. Started Forging enabled successfully in $IP_SERVER"
+                   MG_TEXT="$DELEGATE_NAME reload started. Found fork: $FORK  ---  Forging enabled successfully in $IP_SERVER  $RESPONSE"
+                   echo "Forging enabled in $IP_SERVER > $RESPONSE" >> $BLOCKHEIGHT_LOG
+                else
+                   echo -e "${RED}Forging not enabled!${OFF} - $RESPONSE"
+                   echo "Forging NOT enabled! > $RESPONSE" >> $BLOCKHEIGHT_LOG
+                   MG_SUBJECT="$DELEGATE_NAME reload. Forging not enabled! Found fork"
+                   MG_TEXT="$DELEGATE_NAME reload started. Found fork: $FORK ---  Forging not enabled! $RESPONSE"
+                fi
+
+                curl -s --user "api:$API_KEY" $MAILGUN -F from="$MG_FROM" -F to="$MG_TO" -F subject="$MG_SUBJECT" -F text="$MG_TEXT"
+	echo "Starting reload $TIME" >> $BLOCKHEIGHT_LOG
+	bash lisk.sh reload
+	sleep 60
+}
 
 local_height() {
     ## Get height of this server and see if it's greater or within 4 of the highest
-        CHECKSRV=`curl -s "http://127.0.0.1:8000/api/loader/status/sync"| jq '.height'`
-        if [ -z "$CHECKSRV" ]
-        then
-                CHECKSRV=`curl -s "http://127.0.0.1:8000/api/loader/status/sync"| jq '.height'`
-        fi
+	get_local_height
+
+	is_forked=`tail /home/$USERTOOL/lisk-main/logs/lisk.log -n 20 | grep "Fork"`
+	if[ -n "$is_forked" ]
+	then
+		echo "Found fork: $is_forked"
+		found_fork_alert $is_forked
+	fi
+
         diff=$(( $HEIGHT - $CHECKSRV ))
         if [ "$diff" -gt "4" ]
         then
-                TIME=$(date +"%H:%M") #add for your local time: -d '6 hours ago')
-                echo "Starting reload at $TIME"
-                cd /home/$USERTOOL/lisk-main/
-                bash lisk.sh reload
-                sleep 60
-                echo "Reload finished"
-                CHECKSRV=`curl -s "http://127.0.0.1:8000/api/loader/status/sync"| jq '.height'`
-                if [ -z "$CHECKSRV" ]
-                then
-                        CHECKSRV=`curl -s "http://127.0.0.1:8000/api/loader/status/sync"| jq '.height'`
-                fi
+		start_reload
+		get_local_height
                 diff=$(( $HEIGHT - $CHECKSRV ))
                 ## Rebuild if still out of sync after reload
                 if [ "$diff" -gt "5" ]
                 then
                         rebuild_alert
-                        echo "Rebuilding with heights: " "$CHECKSRV" " -- " "$HEIGHT"
-                        bash lisk.sh rebuild
-                        sleep 900
+                        echo "Rebuilding with heights: Highest: $HEIGHT -- Local: $CHECKSRV"
+                        #bash lisk.sh rebuild
+			find_newest_snap_rebuild
+                        check_if_rebuild_finish
                         post_rebuild
                 fi
         fi
@@ -105,14 +212,20 @@ local_height() {
 
 
 echo "Starting log.. " > $BLOCKHEIGHT_LOG
-echo "Starting v1"
+echo "Starting toolisk"
 while true; do
-        first_node
-        loop_nodes
-        local_height
 	TIME=$(date +"%H:%M") #add for your local time: -d '6 hours ago')
-        echo -e "${GREEN}$CHECKSRV${OFF} -- $HEIGHT -- $TIME"
-        echo "$CHECKSRV -- $HEIGHT -- $TIME" >> $BLOCKHEIGHT_LOG
+	top_height
+	if [ "$HEIGHT" -gt "0" ]
+	then
+	        local_height
+	else
+		echo "Waiting for rebuild.. $TIME"
+	fi
+
+        echo -e "${GREEN}Highest: $HEIGHT${OFF} -- Local: $CHECKSRV -- $TIME"
+        echo "Highest: $HEIGHT -- Local: $CHECKSRV -- $TIME" >> $BLOCKHEIGHT_LOG
         sleep 10
 done
+
 
